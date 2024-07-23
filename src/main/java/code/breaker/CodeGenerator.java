2c487b764c653b373f7ae614c42865bccf04ddc5
kloggenberg023 <kloggenberg023@student.wethinkcode.co.za>
2024-07-23 09:02:26 +0200

    Adding some more game code

---
+++ src/main/java/code/breaker/CodeGenerator.java
@@ -0,0 +1,24 @@
+package code.breaker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CodeGenerator {
+    private static final int MIN_DIGIT = 1;
+    private static final int MAX_DIGIT = 8;
+
+    public List<Integer> createCode() {
+        List<Integer> code = new ArrayList<>();
+        Random random = new Random();
+
+        while (code.size() < GameUtils.CODE_LENGTH) {
+            int value = random.nextInt(MAX_DIGIT - MIN_DIGIT + 1) + MIN_DIGIT;
+            if (!code.contains(value)) {
+                code.add(value);
+            }
+        }
+
+        return code;
+    }
+}
