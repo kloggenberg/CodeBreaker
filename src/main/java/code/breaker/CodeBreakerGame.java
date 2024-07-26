d8702dab90ea3b411ac89c25544a710b85e4a35d
kloggenberg023 <kloggenberg023@student.wethinkcode.co.za>
2024-07-26 11:08:45 +0200

    Adding javadocs CodeBreakerGame

--- src/main/java/code/breaker/CodeBreakerGame.java
+++ src/main/java/code/breaker/CodeBreakerGame.java
@@ -3,7 +3,28 @@ package code.breaker;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Main class for running the Code Breaker game.
+ * <p>
+ * This class is responsible for initializing the game, handling the game loop,
+ * and interacting with the user. It generates a code, takes user guesses,
+ * and provides feedback until the user either guesses the code correctly or
+ * exhausts the maximum number of turns.
+ * </p>
+ */
 public class CodeBreakerGame {
+
+    /**
+     * The entry point of the Code Breaker game.
+     * <p>
+     * Initializes the code generator, game utilities, and game results handler.
+     * It then enters a loop where the user is prompted to enter guesses, and
+     * feedback is provided based on the guesses. The loop continues until the
+     * code is correctly guessed or the maximum number of turns is reached.
+     * </p>
+     *
+     * @param args Command line arguments (not used).
+     */
     public static void main(String[] args) {
         CodeGenerator codeGenerator = new CodeGenerator();
         GameUtils gameUtils = new GameUtils();
