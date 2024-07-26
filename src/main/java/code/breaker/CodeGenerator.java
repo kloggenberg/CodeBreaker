31689c1ecbadd206ce50eee53fdf07a8e385d706
kloggenberg023 <kloggenberg023@student.wethinkcode.co.za>
2024-07-26 11:16:35 +0200

    Adding javadocs CodeGenerator

--- src/main/java/code/breaker/CodeGenerator.java
+++ src/main/java/code/breaker/CodeGenerator.java
@@ -4,10 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Generates a random code for the Code Breaker game.
+ * <p>
+ * This class is responsible for creating a code consisting of a specific number
+ * of unique digits within a defined range. The digits are randomly selected and
+ * ensured to be unique in the generated code.
+ * </p>
+ */
 public class CodeGenerator {
+    /** The minimum digit value that can be included in the code. */
     private static final int MIN_DIGIT = 1;
+
+    /** The maximum digit value that can be included in the code. */
     private static final int MAX_DIGIT = 8;
 
+    /**
+     * Creates a code with unique random digits.
+     * <p>
+     * Generates a list of unique digits, each ranging from {@link #MIN_DIGIT}
+     * to {@link #MAX_DIGIT}. The length of the code is defined by
+     * {@link GameUtils#CODE_LENGTH}. This method ensures that all digits in the
+     * code are unique.
+     * </p>
+     *
+     * @return A list of integers representing the generated code.
+     */
     public List<Integer> createCode() {
         List<Integer> code = new ArrayList<>();
         Random random = new Random();
@@ -22,10 +44,20 @@ public class CodeGenerator {
         return code;
     }
 
+    /**
+     * Gets the minimum digit value.
+     *
+     * @return The minimum digit value that can be used in the code.
+     */
     public static int getMinDigit() {
         return MIN_DIGIT;
     }
 
+    /**
+     * Gets the maximum digit value.
+     *
+     * @return The maximum digit value that can be used in the code.
+     */
     public static int getMaxDigit() {
         return MAX_DIGIT;
     }
